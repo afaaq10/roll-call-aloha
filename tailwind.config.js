7dd1726b7f64737172ad9d8c3b5a2b70d11c8e33
afaaq10 <afaaqmajeed123@gmail.com>
2024-12-11 11:56:14 +0530

    💄 Title font updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{html,js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cooper: ['Cooper Black', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
